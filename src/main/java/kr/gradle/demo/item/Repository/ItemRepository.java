9708b30305a83e0bf8a0126bf77b804a2cfa2b98
bong94688 <bong94688@gmail.com>
2023-06-28 21:17:37 +0900

    스프링 4일차

--- src/main/java/kr/gradle/demo/item/Repository/ItemRepository.java
+++ src/main/java/kr/gradle/demo/item/Repository/ItemRepository.java
@@ -1,10 +1,30 @@
 package kr.gradle.demo.item.Repository;
 
 import kr.gradle.demo.item.entity.Item;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item,Long> {
+
+
+    List<Item> findByItemNm(String ItemNm);
+    List<Item> findByItemNmOrItemDetail(String name,String detail);
+
+
+
+//    Item 이라는 entity 테이블에 i 이라는 별칭을주고
+    @Query("SELECT i FROM Item i where i.itemDetail like %:ItemDetail% "+
+            "order by i.price desc")
+    List<Item> findByItemDetail(@Param("ItemDetail") String ItemDetail);
+
+    @Query(value = "SELECT * FROM item  where item_detail like %:ItemDetail% "+
+            "order by price asc",nativeQuery = true)
+    List<Item> findByItemDetailNative(@Param("ItemDetail") String ItemDetail);
+
 }
--- src/test/java/kr/gradle/demo/item/Repository/ItemRepositoryTest.java
+++ src/test/java/kr/gradle/demo/item/Repository/ItemRepositoryTest.java
@@ -11,11 +11,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
-
+//JUnit
+//inteliJ -> shift + F9
 @SpringBootTest
 class ItemRepositoryTest {
 
@@ -26,6 +29,9 @@ class ItemRepositoryTest {
 
     @PersistenceContext
     EntityManager em;
+
+
+
     @Test
     @DisplayName("상품 저장 테스트")
     public void createItemTest(){
@@ -38,13 +44,70 @@ class ItemRepositoryTest {
         item.setRegTime(LocalDateTime.now());
         item.setUpdateTime(LocalDateTime.now());
         Item savedItem = itemRepository.save(item);
-        System.out.println(savedItem.toString());
+    }
+
+    public void createItemList(){
+
+//        item entity 아이템 채우기
+        for(int i=1;i<=10;i++){
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명"+i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100); item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+
+
+
+
+    @Test
+    @DisplayName("상품명, 상품설명 or 테스트")
+    public void findByItemNmOrItemDetailTest(){
+
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1","테스트 상품 상세 설명10");
+
+        for(Item item: itemList){
+            System.out.println(item.toString());
+        }
+
+    }
+    
+    @Test
+    @DisplayName("JPAL 쿼리")
+    public void findByItemDetailTest(){
+        this.createItemList();
+        
+        List<Item> itemList = itemRepository.findByItemDetail("테스트");
+
+        for(Item item: itemList){
+            System.out.println(item);
+        }
+    }
+
+
+
+    @Test
+    @DisplayName("Native 쿼리")
+    public void findByItemDetailNativeTest(){
+        this.createItemList();
+
+        List<Item> itemList = itemRepository.findByItemDetailNative("테스트");
+
+        for(Item item: itemList){
+            System.out.println(item);
+        }
     }
 
     @Test
-    @DisplayName("JPQL쿼리")
-    public void findByItemDetailTest(){
-
+    @DisplayName("querydsl 테스트2")
+    public void querydslTest(){
 
     }
+
 }
